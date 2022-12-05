c338b7bc0ceed8373c873e723e00de8083de1bf2
yousioph <Yousioph25@gmail.com>
2022-12-05 11:27:00 +0100

    Day 1

---
+++ day1.py
@@ -0,0 +1,4 @@
+print("Welcome to the Band Name Generator")
+city_name = input("What is the name of the city you grew up in? ")
+pet_name = input("What's your pet name? ")
+print("Your band name coud be", city_name, pet_name)
